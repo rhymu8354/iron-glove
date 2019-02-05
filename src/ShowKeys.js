6414b22728a64a464a3c5b59475a0d8421fd31d8
Richard Walters <rwalters@digitalstirling.com>
2019-02-05 13:35:09 -0800

    ShowKeys: remove unneeded constructor

--- src/ShowKeys.js
+++ src/ShowKeys.js
@@ -1,10 +1,6 @@
 import React from "react";
 
 class ShowKeys extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         let report = "Keys: ";
         let first = true;
